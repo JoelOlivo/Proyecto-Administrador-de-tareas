ee59d768df9b8fdfbd872fc943c07806ac267950
JoelOlivo <139314609+JoelOlivo@users.noreply.github.com>
2023-11-22 23:11:39 -0500

    creación de función de crearTodohtml

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import './styles.css';
-// import { Todo } from './class/todo.class.js';
-// import { TodoList } from './class/todo-list.class.js';
+
 import { Todo, TodoList } from "./class";
+import { crearTodoHtml } from './js/componentes';
 
 const todoList = new TodoList();
 
 const tarea = new Todo('Aprender JavaScript');
-const tarea2 = new Todo('Aprender Java');
-
-
 todoList.nuevoTodo( tarea );
 
-todoList.nuevoTodo( tarea2 );
 console.log(todoList);
+crearTodoHtml(tarea);
